a09d1f4476ff6bcfeca552214c52f9154afea30b
aleksej.minka <aleksej.minka@gmail.com>
2021-09-07 00:22:30 +0300

    Add changes for qa branch

--- bash.sh
+++ bash.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 status=$(curl http://$1:8081 -s -o /dev/null -w "%{http_code}")
-until [ $status == 200 ] 
+until [ $status == 200 ]
   do
     sleep 30
     echo "Wait another 30 sec"
     status=$(curl http://$1:8081 -s -o /dev/null -w "%{http_code}")
   done
 sleep 100
+wget http://$1:8081/jnlpJars/jenkins-cli.jar
 PluginsList=(
   git
   github-branch-source
-  matrix-auth
+  role-strategy
   configuration-as-code
   credentials-binding
   ssh-agent
@@ -42,4 +43,4 @@ until [ $status == 200 ]
     echo "Wait another 30 sec"
     status=$(curl http://$1:8081 -s -o /dev/null -w "%{http_code}")
   done
-echo "Jenkins is ready, go ahead to link" http://$1:8081
+echo "Jenkins is ready, go ahead" http://$1:8081
